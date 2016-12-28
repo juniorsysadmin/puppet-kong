09bb03785ded0e4d844974f850bbdd4a6b946009
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2016-12-28 19:00:05 +1100

    Add validate_cmd for Kong configuration file

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,10 +2,11 @@
 class kong::config inherits kong {
 
   file { $kong::config_file_path:
-    ensure  => file,
-    content => template($kong::config_file_template),
-    group   => $kong::config_file_group,
-    mode    => $kong::config_file_mode,
-    owner   => $kong::config_file_owner,
+    ensure       => file,
+    content      => template($kong::config_file_template),
+    group        => $kong::config_file_group,
+    mode         => $kong::config_file_mode,
+    owner        => $kong::config_file_owner,
+    validate_cmd => "${kong::kong_path} check %",
   }
 }
